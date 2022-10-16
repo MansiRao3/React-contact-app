2bdd412819482bc57f192ea1d4f2ed053b44d711
sreeyareddy2102 <91817798+sreeyareddy2102@users.noreply.github.com>
2022-10-16 22:23:20 +0530

    added h2 header
    here are the changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ const Header =() => {
         <div className="ui fixed menu">
         <div className="ui container center">
             <h2>Contact Manager and contactor</h2>
+            <h2>Here are the contacts</h2>
         </div>
         </div>
     );
